bbf3bd915ad47a61532af1a6e33a4543067d2bb4
huanle0610 <735064191@qq.com>
2018-07-15 15:35:09 +0800

    mongodb query method

--- console/controllers/MongoTestController.php
+++ console/controllers/MongoTestController.php
@@ -37,4 +37,41 @@ class MongoTestController extends Controller
 
         var_dump($rows);
     }
+
+    public function actionComplexQuery()
+    {
+
+        $query = new Query();
+        $query->from('inventory')
+            ->andWhere(['status' => 'D'])
+            ->andWhere(['<', 'size.h', 15])
+            ->limit(10);
+// execute the query
+        $rows = $query->all();
+
+        var_dump($rows);
+
+        $query = new Query();
+        $query->from('inventory')
+            ->andWhere(['tags' => ['blank', 'red']])
+            ->limit(10);
+// execute the query
+        $rows = $query->all();
+
+        var_dump($rows);
+    }
+
+    public function actionInsertAll()
+    {
+        $data = <<<EOF
+[
+   { item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
+   { item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "A" },
+   { item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
+   { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
+   { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
+]
+EOF;
+        var_dump(json_decode(trim($data)));
+    }
 }
